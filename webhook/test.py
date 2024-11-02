24e774a0d1a83ae6f93ef80c22202a97de04c9d1
AnkurKumarShukla <80956033+AnkurKumarShukla@users.noreply.github.com>
2024-11-02 19:48:53 +0530

    Update test.py

--- webhook/test.py
+++ webhook/test.py
@@ -1,12 +1,12 @@
 import requests
 import json
 
-url = "https://dimensional-special-wind.quiknode.pro/ed11912a414200cb1a9bc7b68bed8b69f989f007/"
+url = "https://dimensional-special-wind.quiknode.pro/ed119******************/"
 
 payload = json.dumps({
   "method": "bb_getBalanceHistory",
   "params": [
-    "0x9862fd5b8dbfbe4e887d244d28e53d792ded66eb",
+    "0x9862f**************e53d792ded66eb",
     {
       "from": "1683684000",
       "to": "1700042400",
@@ -25,13 +25,13 @@ response = requests.request("POST", url, headers=headers, data=payload)
 
 print(response.json())
 
-# 0x9862fd5b8dbfbe4e887d244d28e53d792ded66eb
+# 0x9862fd5b8db********************ded66eb
 # balance wala address
 
 # from jsonrpcclient import request, parse, Ok
 # import logging
 # import requests
-# response = requests.post("https://dimensional-special-wind.quiknode.pro/ed11912a414200cb1a9bc7b68bed8b69f989f007/", json=request("sc_getAddressAnalysis", { "hash": "0xa7efae728d2936e78bda97dc267687568dd593f3" }))
+# response = requests.post("https://dimensional-special-wind.quiknode.pro/ed1191***************************/", json=request("sc_getAddressAnalysis", { "hash": "0xa7efae728d2936e78bda97dc267687568dd593f3" }))
 # parsed = parse(response.json())
 # if isinstance(parsed, Ok):
 #     print(parsed.result)
